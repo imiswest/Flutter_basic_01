93e9e7f80ca284cd8a28a76e2744bd31d7cf7baf
imiswest <ies9961@naver.com>
2022-12-24 17:16:19 +0900

    ~p.60 Container까지완료

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart'; //material.dart라는 라이브러리 파일을 불러오는 과정
+
+void main() {
+  runApp(const MyApp()); //MyApp이라는 위젯을 실행시킨다
+}
+
+class MyApp extends StatelessWidget { //extends를 통해서 상속
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) { //모든 위젯들이 공통적으로 가지는 메서드(함수)
+    //BuildContext : 모든 위젯이 가지고 있는 고유의 아이디값.
+    // 모든 위젯들이 트리 형태로 구성돼 있을 때 각 위젯의 위치가 BuildContext가 되어 각 위젯마다 갖고 있게 됨
+
+    return MaterialApp( //MaterialApp이라는 메서드(함수)의 프로퍼티(하위내용) - title, theme, home
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green, //전체 테마 색깔
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page') //첫 화면을 나타내는 위젯
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget { //StatelessWidget을 상속받음
+  final String title; //MyHomePage의 프로퍼티
+  //final : 변경하지 않도록 함
+
+  MyHomePage({required this.title}); //생성자
+  //아직 title의 기본값을 선언해주지 않아서 MyHomePage 객체를 생성할 때 꼭 받아와야 하므로 required를 붙임
+  //this는 해당 객체 내의 값에 접근하기 위한 키워드
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold( //Scaffold: 앱의 화면이 기본적으로 갖출 기능들을 선언해둔 위젯
+      appBar: AppBar(
+        title: Text(
+          title,
+        ),
+      ),
+
+      body: Container( /// Container 위젯으로 감싸는 이유 : padding, color(배경색), width&height 프로퍼티가 가능
+        /// (Text 위젯에는 padding 프로퍼티처럼 여백을 주는 기능이 없음)
+        /// (Padding 위젯도 있음. 하지만 Padding 위젯은 프로퍼티로 padding과 child만이 가능)
+        child: Text('Hello, Flutter~'), //Container은 child가 필수인 위젯
+        padding: EdgeInsets.all(30), //Container의 child 위젯에 적용될 여백을 설정할 수 있음
+        ///Padding의 종류 : .fromLTRB(좌,상,우,하 방향)  .only(한 방향에 대해서만 여백)  .symmetric(세로방향, 가로방향에 대한 여백 설정)
+        color: Colors.orange,
+        width: 300,
+        height: 200
+      ),
+
+      // Center(
+      //   child: Text('Hello, Flutter!')
+      // ),
+    );
+  }
+}
+
+
+//프로퍼티는 소문자, 위젯은 대문자
